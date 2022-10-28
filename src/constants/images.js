6c2fea3069386cb7f5e92b419c859271f708695f
rwambui <rosemwangi305@gmail.com>
2022-10-28 15:56:21 +0300

    updated mockup json APIs

---
+++ src/constants/images.js
@@ -0,0 +1,64 @@
+import bg from "../assets/bg.png";
+import chef from "../assets/chef.png";
+import G from "../assets/G.png";
+import pic1 from "../assets/pic1.png";
+import pic2 from "../assets/pic2.png";
+import pic3 from "../assets/pic3.png";
+import pic4 from "../assets/pic4.png";
+import pic5 from "../assets/pic5.png";
+import pic6 from "../assets/pic6.png";
+import pic7 from "../assets/pic7.png";
+import pic8 from "../assets/pic8.png";
+import knife from "../assets/knife.png";
+import logo from "../assets/logo.png";
+import menu from "../assets/menu.png";
+import overlaybg from "../assets/overlaybg.png";
+import spoon from "../assets/spoon.svg";
+// import welcome from "../assets/welcome.png";
+import welcome from "../assets/welcome.png";
+import findus from "../assets/findus.png";
+import laurels from "../assets/laurels.png";
+import award01 from "../assets/award01.png";
+import award02 from "../assets/award02.png";
+import award03 from "../assets/award03.png";
+import award05 from "../assets/award05.png";
+import sign from "../assets/sign.png";
+import quote from "../assets/quote.png";
+import gericht from "../assets/gericht.png";
+import dinemasters0 from "../assets/dinemasters0.svg";
+// import dinemasters from "../assets/dinemasters.png";
+import dinemasters1 from "../assets/dinemasters1.png";
+import dinemasters3 from "../assets/dinemasters3.png";
+
+export default {
+  bg,
+  chef,
+  G,
+  pic1,
+  pic2,
+  pic3,
+  pic4,
+  pic5,
+  pic6,
+  pic7,
+  pic8,
+  knife,
+  logo,
+  menu,
+  overlaybg,
+  spoon,
+  welcome,
+  findus,
+  laurels,
+  award01,
+  award02,
+  award03,
+  award05,
+  sign,
+  quote,
+  gericht,
+  dinemasters0,
+  // dinemasters,
+  dinemasters1,
+  dinemasters3,
+};
